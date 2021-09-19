75671157c1febf81e4a3f4ae34f0c674e634c23b
shuked <shukeds@gmail.com>
2021-09-19 23:17:15 +0300

    added array students in students-array.js

---
+++ students-array.js
@@ -0,0 +1,72 @@
+const students = [
+    {
+        name: "Shaked Shaul",
+        mail: "shukeds@gmail.com",
+        cvFile: "/shaked-shaul/cv.html"
+    },
+    {
+        name: "Matan Vilensky",
+        mail: "matanv@gmail.com",
+        cvFile: "/matan-vilensky/matan-cv.html"
+    },
+    {
+        name: "Omri Dadon",
+        mail: "omridadon@gmail.com",
+        cvFile: "/omri-dadon/CV.html"
+    },
+    {
+        name: "Ortal Tsarfati",
+        mail: "ortalts89@gmail.com",
+        cvFile: "/ortal/cv.html"
+    },
+    {
+        name: "Alex",
+        mail: "sashaa1819@gmail.com",
+        cvFile: "?"
+    },
+    {
+        name: "Maor Lachish",
+        mail: "mlachish1@gmail.com/",
+        cvFile: "/maor-lachish/cv.html"
+    },
+    {
+        name: "Alon Saban",
+        mail: "Alonsaban3@gmail.com",
+        cvFile: "/AlonSaban/cv.html"
+    },
+    {
+        name: "Liron Shushan",
+        mail: "liron08648@gmail.com",
+        cvFile: "?"
+    },
+    {
+        name: "Yasmin Bordiny",
+        mail: "bordiny@gmail.com",
+        cvFile: "?"
+    },
+    {
+        name: "Nadav Shlomi",
+        mail: "NadavSSS@gmail.com",
+        cvFile: "?"
+    },
+    {
+        name: "Ben Vered",
+        mail: "ben.vered@gmail.com",
+        cvFile: "?"
+    },
+    {
+        name: "Erez Vaknin",
+        mail: "erezva86@gmail.com",
+        cvFile: "?"
+    },
+    {
+        name: "Yehuda Or",
+        mail: "yudaor252@gmail.com",
+        cvFile: "?"
+    },
+    {
+        name: "David Levy",
+        mail: "david@davidlevy.co.il",
+        cvFile: "/david-levy/cv.html"
+    },
+]
